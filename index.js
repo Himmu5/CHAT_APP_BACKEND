84c74872479683e174a2267096f0738960c0207a
Himanshu <Himanshuchauhan@gmail.com>
2023-12-02 17:57:34 +0530

    fix : CORS error resolved

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "https://beamish-cheesecake-6369e3.netlify.app",
-      "https://beamish-cheesecake-6369e3.netlify.app/",
-      "http://localhost:5173",
-    ],
+    origin: "*",
     credentials: true,
   })
 );
